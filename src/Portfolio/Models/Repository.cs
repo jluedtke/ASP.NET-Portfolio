a16995c92e34863b0e6a18dd08cda3a1ff97c124
Jared Luedtke <jared7223@hotmail.com>
2017-08-25 10:50:12 -0700

    set up Repo model, initial view pages, and inital controllers

---
+++ src/Portfolio/Models/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Portfolio.Models
+{
+    public class Repository
+    {
+        
+        public static List<Repository> GetRepositories(int number)
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest("search/repositories?q=user:" + EnvironmentVariables.UserName + "&sort=stars&order=desc&page=1&per_page=" + number, Method.GET);
+            request.AddHeader("user-agent", EnvironmentVariables.UserAgent);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            List<Repository> repos = JsonConvert.DeserializeObject<List<Repository>>(jsonResponse["messages"].ToString());
+            return repos;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
--- src/Portfolio/Startup.cs
+++ src/Portfolio/Startup.cs
@@ -7,30 +7,41 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace Portfolio
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseStaticFiles();
             loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async (error) =>
+            {
+                await error.Response.WriteAsync("You should not see this message. An error has occured.");
             });
         }
     }
